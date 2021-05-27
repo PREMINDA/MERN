908204b1d795a7f8c9c243204e41d060f27c1609
PREMINDA <premindabandara0777@gmail.com>
2021-05-27 23:53:30 +0530

    adding new path

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,15 +4,21 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">MERNSHOP</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/cart">Home</Nav.Link>
-            <Nav.Link href="/login">Sign In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">MERNSHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Home
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   );
