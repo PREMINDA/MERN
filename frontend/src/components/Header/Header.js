2ffebf3bf857c466e2fb4d8f81e0df0c5fe967c0
PREMINDA <premindabandara0777@gmail.com>
2021-05-28 23:55:55 +0530

    Product Screen

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -12,12 +12,16 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end" style={{ width: "100%" }}>
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart pr-5"></i> Home
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart pr-5"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="signin">
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
