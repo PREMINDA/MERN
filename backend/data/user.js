91287199be850b353ff43194ce3ba48a673f7a09
PREMINDA <premindabandara0777@gmail.com>
2021-06-09 15:19:14 +0530

    add users to DB

---
+++ backend/data/user.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcryptjs";
+
+const user = [
+  {
+    name: "Admin User",
+    email: "admin@exampl.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "John Doe",
+    email: "john@exampl.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "jane mac",
+    email: "jane@exampl.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
