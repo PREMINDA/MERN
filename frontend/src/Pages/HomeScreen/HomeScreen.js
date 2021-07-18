3f6367f554f819697547285e4174a9a61e053ebc
PREMINDA <premindabandara0777@gmail.com>
2021-07-19 01:08:22 +0530

    Add redux state to HomeScreen

--- frontend/src/Pages/HomeScreen/HomeScreen.js
+++ frontend/src/Pages/HomeScreen/HomeScreen.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import Product from "./../../components/Product/Product";
+import { listProducts } from "../../actions/productActions";
 import axios from "axios";
 
 function HomeScreen(props) {
-  const [products, setProduct] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
 
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchproduct = async () => {
-      const { data } = await axios.get(`/api/products`);
-      setProduct(data);
-    };
-    fetchproduct();
+    dispatch(listProducts());
   }, []);
   return (
     <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
       <h1>Latest Product</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
     </>
   );
 }
